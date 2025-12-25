4a3c9f964f7bd5970c24229f132d520b1d797af9
bhattproject <bhattjirajeev@gmail.com>
2025-12-26 00:07:01 +0530

    Correct syntax and structure in tin.cpp
    Fix syntax errors and improve structure in tin.cpp

---
+++ tin.cpp
@@ -0,0 +1,31 @@
+#inclue<iostream.h>
+#include<string.h>
+#include<bits/stdc++.h>
+include namespace std;
+
+
+
+int main()
+{
+  int c;
+  class ob
+{
+  int value;
+  string name;
+}
+
+
+  cout<<value<<'\n;
+  
+  cin>>value;
+   cout<<name<<'\n;
+  
+  gets>>name;
+
+  int j;
+  plq(ob.j);
+}
+string plq(sys){
+
+  return main();
+}
